c140994d75f9132800ad97880d3942490ce5e5ef
zeroall-h <heodayeong@gmail.com>
2024-05-01 19:44:53 +0900

    해커뉴스 앱 제작

--- frontendAcademy/hackernews/3_두개의화면/app.js
+++ frontendAcademy/hackernews/3_두개의화면/app.js
@@ -3,38 +3,30 @@ const ajax = new XMLHttpRequest();
 const content = document.createElement("div");
 const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
 const CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json";
-function getData(url) {
-  ajax.open("GET", url, false);
-  ajax.send();
 
-  return JSON.parse(ajax.response);
-}
+ajax.open("GET", NEWS_URL, false);
+ajax.send();
 
-const newsFeed = getData(NEWS_URL);
+const newsFeed = JSON.parse(ajax.response);
 const ul = document.createElement("ul");
 
 addEventListener("hashchange", function () {
   const id = location.hash.substr(1);
-  const newsContent = getData(CONTENT_URL.replace("@id", id));
-  const title = this.document.createElement("h1");
+  ajax.open("GET", CONTENT_URL.replace("@id", id), false);
+  ajax.send();
+  const newsContent = JSON.parse(ajax.response);
+  const title = document.createElement("h1");
   title.innerHTML = newsContent.title;
   content.appendChild(title);
 });
 
 for (let i = 0; i < 10; i++) {
-  const div = document.createElement("div");
   const li = document.createElement("li");
   const a = document.createElement("a");
-
-  div.innerHTML = `
-  <li>
-    <a href="#${newsFeed[i].id}">
-      ${newsFeed[i].title} (${newsFeed[i].comments_count})
-    </a>
-  </li>
-  `;
-
-  ul.appendChild(div.firstElementChild);
+  a.href = `#${newsFeed[i].id}`;
+  a.innerHTML = `${newsFeed[i].title} (${newsFeed[i].comments_count})`;
+  li.appendChild(a);
+  ul.appendChild(li);
 }
 container.appendChild(ul);
 container.appendChild(content);
